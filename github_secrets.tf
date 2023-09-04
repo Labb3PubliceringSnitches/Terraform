e0b09cd3886b3275c704198e7c2d41ed3a517b77
Frampt <112397341+IamFrampt@users.noreply.github.com>
2023-09-04 16:25:17 +0200

    Update github_secrets.tf

--- github_secrets.tf
+++ github_secrets.tf
@@ -1,10 +1,4 @@
 variable "AZURE_SUB_ID_SECRET" {}
-data "github_secret" "AZURE_TENANT_ID_SECRET" {
-  secret_name = "AZURE_TENANT"
-}
-data "github_secret" "AZURE_CLIENT_ID_SECRET" {
-  secret_name = "AZURE_CLIENT_ID"
-}
-data "github_secret" "AZURE_CLIENT_SECRET_SECRET" {
-  secret_name = "AZURE_CLIENT_SECRET"
-}
+variable "AZURE_TENANT_ID_SECRET" {}
+variable "AZURE_CLIENT_ID_SECRET" {}
+variable "AZURE_CLIENT_SECRET_SECRET" {}
