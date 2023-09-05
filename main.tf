06dc982ed724613874a2d018b06f914d0ae87c74
Frampt <mikael.eriksson@iths.se>
2023-09-05 14:27:49 +0200

    trying

--- main.tf
+++ main.tf
@@ -1,9 +1,8 @@
-# terraform {
-#   backend "azurerm" {
-#     resource_group_name   = data.local.RGname
-#     storage_account_name  = data.azurerm_storage_account.TFSTATESTORAGE.storage_account_name
-#     container_name        = "statecontainer"
-#     key                   = "terraform.tfstate"
-#     tenant_id             = var.AZURE_TENANT_ID_SECRET
-#   }
-# }
+terraform {
+  backend "azurerm" {
+    resource_group_name   = data.local.RGname
+    storage_account_name  = data.azurerm_storage_account.TFSTATESTORAGE.storage_account_name
+    container_name        = "statecontainer"
+    key                   = "terraform.tfstate"
+  }
+}
