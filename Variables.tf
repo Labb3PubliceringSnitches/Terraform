094a47724cfdc127265263896a3a8483ecd70386
PontusLann <pontus.lann@iths.se>
2023-09-05 09:59:34 +0200

    Major to minor ratio fix

--- Variables.tf
+++ Variables.tf
@@ -2,3 +2,9 @@ variable "AZURE_SUB_ID_SECRET" {}
 variable "AZURE_TENANT_ID_SECRET" {}
 variable "AZURE_CLIENT_ID_SECRET" {}
 variable "AZURE_CLIENT_SECRET_SECRET" {}
+
+variable "create_resource_group" {
+  description = "Set to true to create the resource group, or false to skip creation."
+  type        = bool
+  default     = true 
+}
