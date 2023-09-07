c3cad70dbf2370132c2ca089ece3844c367692dd
Frampt <mikael.eriksson@iths.se>
2023-09-07 09:33:32 +0200

    rewriting

--- backend.tf
+++ backend.tf
@@ -1,8 +1,6 @@
-terraform {
-  backend "azurerm" {
+backend "azurerm" {
     resource_group_name   = "Labb3-Snitches-RG"
     storage_account_name  = "snitchedstate"
     container_name        = "statecontainer"
     key                   = "terraform.tfstate"
-  }
 }
