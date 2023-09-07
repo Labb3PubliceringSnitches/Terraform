ed6c24b7ff9582fb8876f18b90c2243b7b321bb7
Frampt <mikael.eriksson@iths.se>
2023-09-07 14:42:42 +0200

    heheh

--- WebApp.tf
+++ WebApp.tf
@@ -31,7 +31,7 @@ resource "azurerm_linux_web_app" "webapp_snitches" {
 # The code -----------------------------------------------------------------------------------
 resource "azurerm_app_service_source_control" "Production_Code" {
   app_id   = azurerm_linux_web_app.webapp_snitches.id
-  repo_url = "https://github.com/Labb3PubliceringSnitches/PolisApp.git"
+  repo_url = "https://github.com/IamFrampt/PolisApp_Labb2.git"
   branch   = "main"
   depends_on = [ azurerm_linux_web_app.webapp_snitches]
 }
