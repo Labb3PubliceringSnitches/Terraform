7a8b97772059137e4a00c150b6598c9d6b56d5a1
Frampt <mikael.eriksson@iths.se>
2023-09-07 09:37:59 +0200

    test

--- main.tf
+++ main.tf
@@ -1,6 +1,10 @@
+terraform{
+
 backend "azurerm" {
     resource_group_name   = "Labb3-Snitches-RG"
     storage_account_name  = "snitchedstate"
     container_name        = "statecontainer"
     key                   = "terraform.tfstate"
+}
+
 }
