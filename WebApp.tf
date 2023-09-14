bc1ca8df4c73fc56eddfef1376e5b8817da5ac67
Frampt <mikael.eriksson@iths.se>
2023-09-14 11:03:17 +0200

    looooool

--- WebApp.tf
+++ WebApp.tf
@@ -23,6 +23,7 @@ resource "azurerm_linux_web_app" "webapp_snitches" {
   app_settings = {
     "Function_default_key" = local.FA_KEY
     "Function_app_name" = azurerm_windows_function_app.polisapi.name
+    "LA_URL" = local.LA_POST_URL
   }
 
   depends_on = [ azurerm_service_plan.Asp_Snitches,
